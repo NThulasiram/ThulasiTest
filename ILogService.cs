fc5ed580f434442ebc92d5ca61f60c905bff1630
NThulasiram <thulasiram.developer@gmail.com>
2018-01-07 12:12:07 +0530

    Add files via upload

---
+++ ILogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Log4NetLibrary
+{
+    public interface ILogService
+    {
+        void Fatal(string message);
+        void Error(string message);
+        void Warn(string message);
+        void Info(string message);
+        void Debug(string message);
+        void Error(Exception ex);
+    }
+}
